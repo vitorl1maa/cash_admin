1a8eb09203553f1180fea6f1bd83ecf32b086249
Vitor Lima <vitornascimento923@gmail.com>
2023-09-17 17:00:35 -0300

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,8 +10,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma as any),
   providers:[
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialProvider({
       name: "credentials",
