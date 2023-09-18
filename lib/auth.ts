2557cb62cfa9d2747a3be74f7e3ca6649a7fdba1
vitorl1maa <vitornascimento923@gmail.com>
2023-09-18 17:43:35 -0300

    google authentication error google OAuth fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,8 +12,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma as any),
   providers:[
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     CredentialProvider({
       name: "credentials",
