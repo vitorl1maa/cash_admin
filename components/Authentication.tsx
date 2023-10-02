92d1e86fdf1dd215279f8b0c4847b8f8b82411e8
vitorl1maa <vitornascimento923@gmail.com>
2023-10-02 11:52:30 -0300

    banner login

--- components/Authentication.tsx
+++ components/Authentication.tsx
@@ -5,14 +5,11 @@ export default async function Authentication() {
   return (
     <main className="flex h-screen w-screen">
       <section className="w-custom hidden lg:block lg:h-full">
-        <video
-          autoPlay
-          muted
-          loop
+        <img
+          src="dashboard.gif"
+          alt="Login banner"
           className="h-full w-full inset-0 object-cover"
-        >
-          <source src="/Cash Admin.mp4" type="video/mp4" />
-        </video>
+        />
       </section>
       <section className="w-full lg:w-1/2">
         <Login />
